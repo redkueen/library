789117dec406182a69bf0683f3fc70c62db546d2
redkueen <xqh20186@163.com>
2017-10-29 16:04:38 +0800

    空的框架：  添加controllerMapping  使浏览器可以访问

--- src/main/java/com/xqh/library/UserController.java
+++ src/main/java/com/xqh/library/UserController.java
@@ -1,15 +1,22 @@
 package com.xqh.library;
 
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
 public class UserController {
 
+    @RequestMapping("/register")
     public boolean register(){
         return true;
     }
 
+    @RequestMapping("/login")
     public User login(){
         return null;
     }
 
+    @RequestMapping("/query")
     public User query(){
         return null;
     }
