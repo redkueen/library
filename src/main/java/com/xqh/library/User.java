d4d3f0ed3984e2084d9fe48c1aa9f42a7e24b53f
redkueen <xqh20186@163.com>
2017-10-29 16:01:12 +0800

    空的框架：思路

---
+++ src/main/java/com/xqh/library/User.java
@@ -0,0 +1,11 @@
+package com.xqh.library;
+
+public class User {
+   String username;
+   String password;
+   String tel;
+   String address;
+   String email;
+   int age;
+   boolean sex;
+}
---
+++ src/main/java/com/xqh/library/UserController.java
@@ -0,0 +1,16 @@
+package com.xqh.library;
+
+public class UserController {
+
+    public boolean register(){
+        return true;
+    }
+
+    public User login(){
+        return null;
+    }
+
+    public User query(){
+        return null;
+    }
+}
