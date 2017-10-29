56a7c6f52ce6f2b2698b1c89669140d76f9e14ff
redkueen <xqh20186@163.com>
2017-10-29 21:39:40 +0800

    后端代码的编写：后端和前端接通，也就是说可以接受前端发送的数据

--- src/main/java/com/xqh/library/User.java
+++ src/main/java/com/xqh/library/User.java
@@ -1,6 +1,7 @@
 package com.xqh.library;
 
 public class User {
+   int userId;
    String username;
    String password;
    String tel;
@@ -8,4 +9,68 @@ public class User {
    String email;
    int age;
    boolean sex;
+
+   public int getUserId() {
+      return userId;
+   }
+
+   public void setUserId(int userId) {
+      this.userId = userId;
+   }
+
+   public String getUsername() {
+      return username;
+   }
+
+   public void setUsername(String username) {
+      this.username = username;
+   }
+
+   public String getPassword() {
+      return password;
+   }
+
+   public void setPassword(String password) {
+      this.password = password;
+   }
+
+   public String getTel() {
+      return tel;
+   }
+
+   public void setTel(String tel) {
+      this.tel = tel;
+   }
+
+   public String getAddress() {
+      return address;
+   }
+
+   public void setAddress(String address) {
+      this.address = address;
+   }
+
+   public String getEmail() {
+      return email;
+   }
+
+   public void setEmail(String email) {
+      this.email = email;
+   }
+
+   public int getAge() {
+      return age;
+   }
+
+   public void setAge(int age) {
+      this.age = age;
+   }
+
+   public boolean isSex() {
+      return sex;
+   }
+
+   public void setSex(boolean sex) {
+      this.sex = sex;
+   }
 }
--- src/main/java/com/xqh/library/UserController.java
+++ src/main/java/com/xqh/library/UserController.java
@@ -1,23 +1,39 @@
 package com.xqh.library;
 
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 public class UserController {
 
-    @RequestMapping("/register")
-    public boolean register(){
+
+    /** 访问 register的方式
+    curl -H "Content-Type: application/json" -X POST -d '{
+"userId":"111",
+"username":"cgl",
+"password":"11111",
+"tel":"13064570402",
+"email":"shcaoguilin@163.com",
+"age":30,
+"sex":"true"
+}' http://10.1.2.103:8080/register
+     */
+
+    @RequestMapping(value="/register", method = RequestMethod.POST)
+    public boolean register(@RequestBody  User user){
         return true;
     }
 
+
+    //  访问longin的方式  http://10.1.2.103:8080/login?unm=xuqiaohong&pwd=123
     @RequestMapping("/login")
-    public User login(){
+    public User login(@RequestParam("unm") String username,@RequestParam("pwd") String password){
         return null;
     }
 
+//  访问query的方式  http://10.1.2.103:8080/query?uid=666
+
     @RequestMapping("/query")
-    public User query(){
+    public User query(@RequestParam("uid") int userId){
         return null;
     }
 }
